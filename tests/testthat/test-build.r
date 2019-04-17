9118e21408211868a9f5efd080334b0b0b4bb83c
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-04-17 13:41:35 +0200

    there were 3 assertthat versions

--- tests/testthat/test-build.r
+++ tests/testthat/test-build.r
@@ -70,7 +70,7 @@ test_that("List tarballs for a package", {
 test_that("", {
   need_pkgs(c("assertthat", "testthat", "igraph0"))
   desc <- get_descriptions("assertthat")
-  expect_equal(nrow(desc), 2)
+  expect_equal(nrow(desc), 3)
   expect_true(all(c("Package", "Version", "Title") %in% colnames(desc)))
 
   desc2 <- get_descriptions("igraph0")
