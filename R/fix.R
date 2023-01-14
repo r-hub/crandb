b9e1cd81d3465fe8773d59aa6a482d433d64a4a3
Gábor Csárdi <csardi.gabor@gmail.com>
2023-01-14 11:07:44 +0100

    Refactor archived fixer
    To be able to fix a single package.

--- R/fix.R
+++ R/fix.R
@@ -5,27 +5,32 @@ fix_mistake <- function(start = NULL) {
   if (!is.null(start) && start %in% pkgs) {
     pkgs <- pkgs[match(start, pkgs):length(pkgs)]
   }
-  
+
   for (idx in seq_along(pkgs)) {
     pkg <- pkgs[[idx]]
     message(idx, ". ", pkg)
-    url <- paste0("https://crandb.r-pkg.org:2053/cran/", pkg)
-    res <- httr::GET(url)
-    if (httr::status_code(res) == 404) {
-      message("missing, skipped")
-      next
-    }
-    httr::stop_for_status(res)
-    json <- httr::content(res, as = "text", encoding = "UTF-8")
-    json <- sub(",\"archived\":true", "", json)
-    json <- sub(",\"archived\":\"2023.01.13T..:..:.....:..\"", "", json)
-
-    data <- charToRaw(json)
-    httr::stop_for_status(httr::PUT(
-      url,
-      body = data,
-      encode = "raw",
-      authenticate(Sys.getenv("COUCHDB_USER"), Sys.getenv("COUCHDB_PASSWORD"))
-    ))
+    fix_mistake_package(pkg)
   }
+}
+
+fix_mistake_package <- function(pkg) {
+  url <- paste0("https://crandb.r-pkg.org:2053/cran/", pkg)
+  res <- httr::GET(url)
+  if (httr::status_code(res) == 404) {
+    message("missing, skipped")
+    return()
+  }
+  httr::stop_for_status(res)
+  json <- httr::content(res, as = "text", encoding = "UTF-8")
+  if (!grepl("archived", json)) return()
+  json <- sub(",\"archived\":true", "", json)
+  json <- sub(",\"archived\":\"2023.01.13T..:..:.....:..\"", "", json)
+
+  data <- charToRaw(json)
+  httr::stop_for_status(httr::PUT(
+    url,
+    body = data,
+    encode = "raw",
+    authenticate(Sys.getenv("COUCHDB_USER"), Sys.getenv("COUCHDB_PASSWORD"))
+  ))
 }
